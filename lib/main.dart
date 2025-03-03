fa4c938cddb8c2be76f628a9083d807538404599
elsankry02 <githup800900@gmail.com>
2025-03-03 19:19:42 +0200

    flutter pub add riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weather_app/app.dart';
 
 void main() {
-  runApp(const WeatherApp());
+  runApp(const ProviderScope(child: WeatherApp()));
 }
